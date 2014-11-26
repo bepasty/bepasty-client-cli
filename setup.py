44a6a80504b086f68dec31b8bdc2c80355ac9633
Dennis Schmalacker <github@progde.de>
2014-11-26 21:25:56 +0100

    Introduce rudimentary setup.py for easier installation

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bepasty-client-cli',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/bepasty/bepasty-client-cli',
+    license='BSD 2-clause',
+    author='Dennis Schmalacker',
+    author_email='github@progde.de',
+    description='Command Line Client for the Bepasty Pastebin',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console,"
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Communications :: File Sharing",
+        "Topic :: System :: Archiving :: Backup"
+    ]
+
+)
